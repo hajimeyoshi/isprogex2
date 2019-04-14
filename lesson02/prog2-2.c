9bd07ef37debc25302edcadc593f18463ace3ddd
hajimemb <hajime1998@gmail.com>
2019-04-15 00:18:26 +0900

    prog2-2

--- lesson02/prog2-2.c
+++ lesson02/prog2-2.c
@@ -15,15 +15,29 @@ int *reverse(int *data, int num);
 
 void print(int *data, int num)
 {
+  int i;
+  for(i = 0; i < num; i++){
+    printf("%d ",*(data+i));
+  }
 }
 
 int get(int *data, int num, int index)
 {
+  if (num > index){
+    return *(data+index);
+  }else
     return -1;
 }
 
 int sum(int *data, int num)
 {
+  int i;
+  int sum = 0;
+  for(i = 0; i < num; i++)
+    sum += *(data+i);
+  if(sum > 0)
+    return sum;
+  else
     return -1;
 }
 
