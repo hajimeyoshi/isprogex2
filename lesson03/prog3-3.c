17d446e178e6884142ea67c5bb45abdd78bfcc39
hajimemb <hajime1998@gmail.com>
2019-04-22 20:01:46 +0900

    lesson3-3

--- lesson03/prog3-3.c
+++ lesson03/prog3-3.c
@@ -32,32 +32,71 @@ struct element *get_from_list(struct list *list, int index);
 
 struct list *create_list()
 {
-    return NULL;
+  struct list *e = (struct list*)malloc(sizeof(struct list));
+  e->top = NULL;
+  return e;
 }
 
 struct element *create_element(int value)
 {
-    return NULL;
+  struct element *e = (struct element*)malloc(sizeof(struct element));
+  e->value = value;
+  e->next = NULL;
+  return e;
 }
 
 void print_list(struct list *list)
 {
+  struct element *p;
+  p = list->top;
+  while(p != NULL){
+    printf("%d ",p->value);
+    p = p->next;
+  }
 }
 
 void insert_front(struct list *list, struct element *elem)
 {
+  elem->next = list->top;
+  list->top = elem;
 }
 
 void insert_rear(struct list *list, struct element *elem)
 {
+  if(list->top == NULL){
+    elem->next = list->top;
+    list->top = elem;
+  }else{
+    struct element *p;
+    p = list->top;
+    while(p->next != NULL){
+      p = p->next;
+    }
+    p->next = elem;
+  }
 }
 
 void delete_front(struct list *list)
 {
+  if(list->top != NULL){
+    list->top = list->top->next;
+  }
 }
 
 void delete_rear(struct list *list)
 {
+  if(list->top != NULL){
+    if(list->top->next == NULL){
+      list->top = list->top->next;
+    }else{
+      struct element *p;
+      p = list->top;
+      while(p->next->next != NULL){
+        p = p->next;
+      }
+      p->next = NULL;
+    }
+  }
 }
 
 int size_of_list(struct list *list)
