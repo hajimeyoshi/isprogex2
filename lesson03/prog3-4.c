49cb51d88714477855284b8f1338231d95af79b1
hajimemb <hajime1998@gmail.com>
2019-04-24 11:32:46 +0900

    lesson3

--- lesson03/prog3-4.c
+++ lesson03/prog3-4.c
@@ -33,47 +33,276 @@ struct element *get_from_list(struct list *list, int index);
 
 struct list *create_list()
 {
-    return NULL;
+  struct list *e = (struct list*)malloc(sizeof(struct list));
+  e->top = NULL;
+  e->rear = NULL;
+  e->size = size_of_list(e);
+  return e;
 }
 
 struct element *create_element(int value)
 {
-    return NULL;
+  struct element *e = (struct element*)malloc(sizeof(struct element));
+  e->value = value;
+  e->next = NULL;
+  return e;
 }
 
 void print_list(struct list *list)
 {
+  struct element *p;
+  p = list->top;
+  while(p != NULL){
+    printf("%d ",p->value);
+    p = p->next;
+  }
+  printf("\n");
 }
 
 void insert_front(struct list *list, struct element *elem)
 {
+  if(list->size == 0){
+      list->rear = elem;
+  }  
+  elem->next = list->top;
+  list->top = elem;
+  list->size = size_of_list(list);
 }
 
 void insert_rear(struct list *list, struct element *elem)
-{
+{  
+  if(list->top == NULL){
+    insert_front(list,elem);
+  }else{
+    list->rear->next = elem;
+    list->rear = elem;
+  }
+  list->size = size_of_list(list);
 }
 
 void delete_front(struct list *list)
 {
+  
+  if(list->top != NULL){
+    list->top = list->top->next;
+  }
+  if(list->top == NULL){
+      list->rear = NULL;
+  }
+  list->size = size_of_list(list);
 }
 
 void delete_rear(struct list *list)
 {
+  if(list->top != NULL){
+    if(list->top->next == NULL){
+      delete_front(list);
+    }else{
+      struct element *p;
+      p = list->top;
+      while(p->next->next != NULL){
+        p = p->next;
+      }
+      p->next = NULL;
+      list->rear = p;
+    }
+  }
+  list->size = size_of_list(list);
 }
 
 int size_of_list(struct list *list)
 {
-    return 0;
+  int num = 0;
+  struct element *p;
+  p = list->top;
+  while(p != NULL){
+    p = p->next;
+    num++; 
+  }
+  return num;
 }
 
 struct element *get_from_list(struct list *list, int index)
 {
+  int i;
+  struct element *p;
+  if(list->size <= index){
     return NULL;
+  }
+
+  p = list->top;
+  for(i = 0; i < index; i++){
+    p = p->next;
+  }
+  return p;
 }
 
 /*=============================================*/
 
+void test1()
+{
+  struct list *list = create_list();
+    
+  struct element *e1 = create_element(10);
+  struct element *e2 = create_element(20);
+  struct element *e3 = create_element(50);
+  struct element *e4 = create_element(60);
+    
+  insert_rear(list,e1);
+  insert_rear(list,e2);
+  insert_rear(list,e3);
+  insert_rear(list,e4);
+
+  assert(list->size == 4);
+
+  print_list(list);
+  printf("number of element: %d\n",list->size);
+  printf("Success: %s\n", __func__);
+}
+
+void test2()
+{
+  struct element *p;
+  struct list *list = create_list();
+  int index;
+  index = 2;
+    
+  struct element *e1 = create_element(10);
+  struct element *e2 = create_element(20);
+  struct element *e3 = create_element(50);
+  struct element *e4 = create_element(60);
+    
+  insert_rear(list,e1);
+  insert_rear(list,e2);
+  insert_rear(list,e3);
+  insert_rear(list,e4);
+    
+  p = get_from_list(list,index);
+  assert(p->value == 50);
+
+  print_list(list);
+  printf("value of element (index: %d): %d\n",index,p->value);
+  printf("Success: %s\n", __func__);
+}
+
+void test3()
+{
+  struct element *p;
+  struct list *list = create_list();
+  int index;
+  index = 4;
+    
+  struct element *e1 = create_element(10);
+  struct element *e2 = create_element(20);
+  struct element *e3 = create_element(50);
+  struct element *e4 = create_element(60);
+    
+  insert_rear(list,e1);
+  insert_rear(list,e2);
+  insert_rear(list,e3);
+  insert_rear(list,e4);
+    
+  p = get_from_list(list,index);
+  assert(p == NULL);
+
+  print_list(list);
+  printf("value of element (index: %d): %s\n",index,p);
+  printf("Success: %s\n", __func__);
+}
+
+void test4()
+{
+    struct list *list = create_list();
+
+    struct element *e1 = create_element(10);
+    struct element *e2 = create_element(20);
+
+    insert_front(list, e1);
+    insert_front(list, e2);
+
+    assert(list->top->value == 20);
+    assert(list->top->next->value == 10);
+
+    print_list(list);
+    printf("Success: %s\n", __func__);
+}
+
+void test5()
+{
+  struct list *list = create_list();
+    
+  struct element *e1 = create_element(10);
+  struct element *e2 = create_element(20);
+  struct element *e3 = create_element(50);
+  struct element *e4 = create_element(60);
+    
+  insert_rear(list,e1);
+  insert_rear(list,e2);
+  insert_rear(list,e3);
+  insert_rear(list,e4);
+    
+  assert(list->top->value == 10);
+  assert(list->top->next->value == 20);
+  assert(list->top->next->next->value == 50);
+  assert(list->top->next->next->next->value == 60);
+
+  print_list(list);
+  printf("Success: %s\n", __func__);
+}
+
+void test6()
+{
+  struct list *list = create_list();
+  struct element *e1 = create_element(10);
+  struct element *e2 = create_element(20);
+  struct element *e3 = create_element(50);
+  struct element *e4 = create_element(60);
+
+  insert_rear(list,e1);
+  insert_rear(list,e2);
+  insert_rear(list,e3);
+  insert_rear(list,e4);
+
+  delete_front(list);
+  assert(list->top->value == 20);
+  assert(list->top->next->value == 50);
+  assert(list->top->next->next->value == 60);
+
+  print_list(list);
+  printf("Success: %s\n", __func__);
+}
+
+void test7()
+{
+  struct list *list = create_list();
+  struct element *e1 = create_element(10);
+  struct element *e2 = create_element(20);
+  struct element *e3 = create_element(50);
+  struct element *e4 = create_element(60);
+
+  insert_rear(list,e1);
+  insert_rear(list,e2);
+  insert_rear(list,e3);
+  insert_rear(list,e4);
+
+  delete_rear(list);
+  assert(list->top->value == 10);
+  assert(list->top->next->value == 20);
+  assert(list->top->next->next->value == 50);
+  assert(list->top->next->next->next == NULL);
+
+  print_list(list);
+  printf("Success: %s\n", __func__);
+}
+
 int main()
 {
-    return 0;
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+  test6();
+  test7();
+  return 0;
 }
