24b6a453fe5d13f5fe42d16b739a8dc659ae5148
hajimemb <hajime1998@gmail.com>
2019-04-29 15:39:00 +0900

    prog4-5

--- lesson04/prog4-5.c
+++ lesson04/prog4-5.c
@@ -27,20 +27,38 @@ void delete(struct list *list, struct element *pos);
 
 struct list *create_list()
 {
-    return NULL;
+  struct list *e = (struct list*)malloc(sizeof(struct list));
+  e->top = NULL;
+  return e;
 }
 
 struct element *create_element(int value)
 {
-    return NULL;
+  struct element *e = (struct element*)malloc(sizeof(struct element));
+  e->value = value;
+  e->next = NULL;
+  return e;
 }
 
 void print_list(struct list *list, struct element *pos)
 {
+  struct element *p;
+  p = list->top;
+  while(p != list->top){
+    printf("%d ",p->value);
+    p = p->next;
+  }
 }
 
 void insert_front(struct list *list, struct element *elem)
 {
+  struct element *p;
+  elem->next = list->top;
+  list->top = elem;
+  while(p->next != list->top){
+    p = p->next;
+  }
+  p->next = elem;
 }
 
 void insert_after(struct list *list, struct element *pos, struct element *elem)
