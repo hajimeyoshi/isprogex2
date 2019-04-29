c082574c7006ab72fac7520f1b07eed0ab8cfd1a
hajimemb <hajime1998@gmail.com>
2019-04-29 11:07:44 +0900

    prog4-1

--- lesson04/prog4-1.c
+++ lesson04/prog4-1.c
@@ -26,20 +26,41 @@ void print_reverse_list(struct list *list);
 
 struct list *create_list()
 {
-    return NULL;
+  struct list *e = (struct list*)malloc(sizeof(struct list));
+  e->top = NULL;
+  e->rear = NULL;
+  return e;
 }
 
 struct element *create_element(int value)
 {
-    return NULL;
+  struct element *e = (struct element*)malloc(sizeof(struct element));
+  e->value = value;
+  e->prev = NULL;
+  e->next = NULL;
+  return e;
 }
 
 void print_list(struct list *list)
 {
+  struct element *p;
+  p = list->top;
+  while(p != NULL){
+    printf("%d ",p->value);
+    p = p->next;
+  }
+  printf("\n");
 }
 
 void print_reverse_list(struct list *list)
 {
+  struct element *p;
+  p = list->rear;
+  while(p != NULL){
+    printf("%d ",p->value);
+    p = p->prev;
+  }
+  printf("\n");
 }
 
 /*=============================================*/
@@ -69,9 +90,40 @@ void test1()
     printf("Success: %s\n", __func__);
 }
 
+void test2()
+{
+    struct list *list = create_list();
+    
+    struct element *e1 = create_element(10);
+    struct element *e2 = create_element(20);
+    struct element *e3 = create_element(30);
+
+    list->top = e1;
+    e1->next = e2;
+    e1->prev = NULL;
+    e2->next = e3;
+    e2->prev = e1;
+    e3->next = NULL;
+    e3->prev = e2;
+    list->rear = e3;
+    
+    assert(list->top->value == 10);
+    assert(list->top->next->value == 20);
+    assert(list->top->next->next->value == 30);
+    assert(list->rear->value == 30);
+    assert(list->rear->prev->value == 20);
+    assert(list->rear->prev->prev->value == 10);
+
+    print_list(list);
+    printf("----\n");
+    print_reverse_list(list);
+    printf("Success: %s\n", __func__);
+}
+
 int main()
 {
     test1();
+    test2();
 
     return 0;
 }
