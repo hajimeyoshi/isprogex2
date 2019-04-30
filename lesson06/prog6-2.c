13ef83a21a72439701579fc55c433eee7aa6cd91
hajimemb <hajime1998@gmail.com>
2019-04-30 18:55:37 +0900

    prog6-2

--- lesson06/prog6-2.c
+++ lesson06/prog6-2.c
@@ -26,41 +26,163 @@ int is_empty(struct stack *stack);
 
 struct stack *create_stack()
 {
-    return NULL;
+    struct stack *new;
+
+    new = (struct stack *)malloc(sizeof(struct stack));
+    new->top = 0;
+    return new;
 }
 
 void print_stack(struct stack *stack)
 {
+    int i;
+
+    printf("stack contain: ");
+    for(i = 0; i < stack->top; i++){
+        printf("%d ",stack->data[i]);
+    }
+    printf("\n");
 }
 
 int size_of_stack(struct stack *stack)
 {
-    return 0;
+    return stack->top;
 }
 
 int push(struct stack *stack, int value)
 {
-    return 0;
+    if(stack->top < STACK_SIZE){
+        stack->data[stack->top] = value;
+        stack->top++;
+        return 1;
+    }else{
+        return 0;
+    }
 }
 
 int pop(struct stack *stack)
 {
-    return -1;
+    int num;
+
+    if(stack->top > 0){
+        num = stack->data[stack->top-1];
+        stack->top--;
+        return num;
+    }else{
+        return -1;
+    }
 }
 
 int peek(struct stack *stack)
 {
-    return -1;
+    int num;
+
+    if(stack->top > 0){
+        num = stack->data[stack->top-1];
+        return num;
+    }else{
+        return -1;
+    }
 }
 
 int is_empty(struct stack *stack)
 {
-    return 0;
+    if(stack->top > 0){
+        return 0;
+    }else{
+        return 1;
+    }
 }
 
 /*=============================================*/
 
+void test1()
+{
+    struct stack *stack = create_stack();
+    int i;
+    assert(push(stack,1) == 1);
+    assert(size_of_stack(stack) == 1);
+
+    for(i = 1; i < STACK_SIZE; i++){
+        push(stack,i+1);
+    }
+    assert(push(stack,31) == 0);
+    print_stack(stack);
+
+    printf("Success: %s\n", __func__);
+}
+
+void test2()
+{
+    struct stack *stack = create_stack();
+    int i;
+
+    for(i = 0; i < 5; i++){
+        push(stack,i+1);
+    }
+    print_stack(stack);
+    assert(pop(stack) == 5);
+    
+    for(i = 0; i < 4; i++){
+        pop(stack);
+    }
+    print_stack(stack);
+    assert(pop(stack) == -1);
+
+    printf("Success: %s\n", __func__);
+}
+
+void test3()
+{
+    struct stack *stack = create_stack();
+    int i;
+
+    for(i = 0; i < 5; i++){
+        push(stack,i+1);
+    }
+    print_stack(stack);
+    assert(peek(stack) == 5);
+    printf("peek of stack is %d\n",peek(stack));
+    
+    for(i = 0; i < 5; i++){
+        pop(stack);
+    }
+    print_stack(stack);
+    assert(peek(stack) == -1);
+    printf("stack is empty\n");
+
+    printf("Success: %s\n", __func__);
+}
+
+void test4()
+{
+    struct stack *stack = create_stack();
+    int i;
+
+    for(i = 0; i < 5; i++){
+        push(stack,i+1);
+    }
+    print_stack(stack);
+    assert(is_empty(stack) == 0);
+    printf("stack is not empty\n");
+    
+    for(i = 0; i < 5; i++){
+        pop(stack);
+    }
+    print_stack(stack);
+    assert(is_empty(stack) == 1);
+    printf("stack is empty\n");
+
+    printf("Success: %s\n", __func__);
+}
+
 int main()
 {
+    test1();
+    test2();
+    test3();
+    test4();
+
     return 0;
 }
+
