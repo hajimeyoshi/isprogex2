b0becb5a22e1de13bc30781fea9838f05a331b13
hajimemb <hajime1998@gmail.com>
2019-05-27 16:00:38 +0900

    prog7-1

--- lesson07/prog7-1.c
+++ lesson07/prog7-1.c
@@ -30,45 +30,166 @@ int is_empty(struct stack *stack);
 
 struct stack *create_stack()
 {
-    return NULL;
+    struct stack *new;
+    
+    new = (struct stack *)malloc(sizeof(struct stack));
+    new->top = NULL;
+    new->size = 0;
+    return new;
 }
 
 struct element *create_element(int value)
 {
-    return NULL;
+    struct element *e;
+
+    e = (struct element*)malloc(sizeof(struct element));
+    e->value = value;
+    e->next = NULL;
+    return e;
 }
 
 void print_stack(struct stack *stack)
 {
+    struct element *p;
+
+    printf("Stack contain ");
+    p = stack->top;
+    while(p != NULL){
+        printf("%d",p->value);
+        p = p->next;
+    }
+    printf("\n");
 }
 
 int size_of_stack(struct stack *stack)
 {
-    return 0;
+    struct element *p;
+    int num;
+
+    num = 0;
+    p = stack->top;
+    while(p != NULL){
+        num++;
+        p = p->next;
+    }
+    return num;
 }
 
 void push(struct stack *stack, struct element *elem)
 {
+    elem->next = stack->top;
+    stack->top = elem;
+    stack->size++;
 }
 
 struct element *pop(struct stack *stack)
 {
-    return NULL;
+    struct element *e;
+
+    e = stack->top;
+    if(e != NULL){
+        stack->top = stack->top->next;
+        stack->size--;
+    }
+    return e;
 }
 
 struct element *peek(struct stack *stack)
 {
-    return NULL;
+    return stack->top;
 }
 
 int is_empty(struct stack *stack)
 {
-    return 0;
+    if(stack->size == 0){
+        return 1;
+    }else{
+        return 0;
+    }
 }
 
 /*=============================================*/
 
+void test1()
+{
+    struct stack *stack = create_stack();
+    int i;
+    struct element *e = create_element(10);
+
+    push(stack,e);
+    assert(stack->top->value == 10);
+    assert(size_of_stack(stack) == 1);
+
+    printf("Success: %s\n", __func__);
+}
+
+void test2()
+{
+    struct stack *stack = create_stack();
+    struct element *e1 = create_element(10);
+    struct element *e2 = create_element(20);
+    struct element *p;
+
+    push(stack,e1);
+    push(stack,e2);
+    print_stack(stack);
+    p = pop(stack);
+    assert(p->value == 20);
+    
+    pop(stack);
+    print_stack(stack);
+    assert(pop(stack) == NULL);
+
+    printf("Success: %s\n", __func__);
+}
+
+void test3()
+{
+    struct stack *stack = create_stack();
+    struct element *e1 = create_element(10);
+    struct element *e2 = create_element(20);
+    struct element *p;
+    struct element *pk;
+
+    push(stack,e1);
+    push(stack,e2);
+    print_stack(stack);
+    p = pop(stack);
+    assert(p->value == 20);
+    
+    pk = peek(stack);
+    print_stack(stack);
+    assert(pk->value == 10);
+
+    printf("Success: %s\n", __func__);
+}
+
+void test4()
+{
+    struct stack *stack = create_stack();
+    struct element *e1 = create_element(10);
+    struct element *e2 = create_element(20);
+    struct element *p;
+
+    push(stack,e1);
+    push(stack,e2);
+    print_stack(stack);
+    p = pop(stack);
+    assert(is_empty(stack) == 0);
+    
+    pop(stack);
+    print_stack(stack);
+    assert(is_empty(stack) == 1);
+
+    printf("Success: %s\n", __func__);
+}
+
 int main()
 {
+    test1();
+    test2();
+    test3();
+    test4();
+
     return 0;
 }
